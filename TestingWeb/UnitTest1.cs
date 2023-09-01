99b7e7474be381fefdad55955199280c6d140440
Miguel <migueltronit@gmail.com>
2023-09-01 10:25:10 +0200

    Testing deploy

---
+++ TestingWeb/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestingWeb
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Multiplicar()
+        {
+            WebTest.Logic logic = new WebTest.Logic();
+
+            int result = logic.Multiplicar(5, 5);
+            Assert.AreEqual(result, 25);
+        }
+    }
+}
---
+++ WebTest/Logic.cs
@@ -0,0 +1,11 @@
+﻿namespace WebTest
+{
+    public class Logic
+    {
+        public int Multiplicar(int valor1,int valor2)
+        {
+            
+            return valor1 * valor2;
+        }
+    }
+}
